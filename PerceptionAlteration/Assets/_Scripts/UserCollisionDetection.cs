a416ddb2c1a819bc25eb770a3c85bb2630a35310
Wall <40182161@live.napier.ac.uk>
2016-06-13 12:36:07 +0100

    Upside down added

--- PerceptionAlteration/Assets/_Scripts/UserCollisionDetection.cs
+++ PerceptionAlteration/Assets/_Scripts/UserCollisionDetection.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 // determine how to scale on update
-public enum scaleMode { stopped, shrinking, growing, resetting, growTest };
+public enum scaleMode { stopped, shrinking, growing, resetting, growTest, turning };
 
 
 public class UserCollisionDetection : MonoBehaviour {
@@ -23,6 +23,8 @@ public class UserCollisionDetection : MonoBehaviour {
     // TODO: need to still see about translation when scaling.
     Vector3 playerPosition;
     Vector3 newPos;
+
+    private Vector3 elevation;
 
     // teleport target
     public GameObject teleTarget;
@@ -91,6 +93,18 @@ public class UserCollisionDetection : MonoBehaviour {
 
                 break;
 
+            case scaleMode.turning:
+
+                Debug.Log("turning");
+
+                float theta = Mathf.PI / 2f;
+
+                cameraParent.transform.localRotation = Quaternion.Slerp(cameraParent.transform.localRotation, new Quaternion(Mathf.Sin(theta), 0, 0, Mathf.Cos(theta)), 1.5f* Time.deltaTime);    
+
+                cameraParent.transform.localPosition = Vector3.Slerp(cameraParent.transform.localPosition, elevation, 1.5f* Time.deltaTime);
+
+                break;
+
             // if resetting to middle size
             case scaleMode.resetting:
 
@@ -148,6 +162,31 @@ public class UserCollisionDetection : MonoBehaviour {
                 currentScale = scaleMode.growing;
 
                // newPos = new Vector3(playerPosition.x + 10f, playerPosition.y, playerPosition.z - 10f);
+            }
+
+            // change to red
+            other.gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+
+        }
+
+
+        // UPSIDE DOWN!
+        if (other.gameObject.CompareTag("Perception-Changer-Reset"))
+        {
+            // User is inside large ball
+            Debug.Log("Enemy touch");
+
+
+            // change scale of room
+            //  if (propParent.transform.localScale.x < bigScale.x)
+            {
+
+                currentScale = scaleMode.turning;
+                elevation = new Vector3(0f, 3.2f, 0f);
+                elevation += cameraParent.transform.localPosition;
+
+
+                // newPos = new Vector3(playerPosition.x + 10f, playerPosition.y, playerPosition.z - 10f);
             }
 
             // change to red
