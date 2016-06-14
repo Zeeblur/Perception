8fba94d1fe6c82511ccd55969acd666eea841018
Wall <40182161@live.napier.ac.uk>
2016-06-14 16:35:53 +0100

    Start of new scene with items following nav mesh.

---
+++ PerceptionAlteration/Assets/_Scripts/Test/Controller.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour
+{   
+    private GameObject player;
+    private UserCollisionDetection playerScript;
+    private SteamVR_TrackedObject trackedObj;
+    private GameObject enemyContainer;
+    private EnemyGenerator enemyScript;
+
+    private Valve.VR.EVRButtonId gripBtn = Valve.VR.EVRButtonId.k_EButton_Grip;
+    public bool gripDown = false;
+    public bool gripUp = false;
+    public bool gripPressed = false;
+
+    private Valve.VR.EVRButtonId triggerBtn = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+    public bool triggerDown = false;
+    public bool triggerUp = false;
+    public bool triggerPressed = false;
+
+    private SteamVR_Controller.Device myController { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+
+    void Start()
+    {
+        // initialise controller objects 
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponent<UserCollisionDetection>();
+        enemyContainer = GameObject.FindGameObjectWithTag("Respawn");
+        enemyScript = enemyContainer.GetComponent<EnemyGenerator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check if controller is null or not first
+        if (myController == null)
+        {
+            Debug.LogError("Controller not found!!");
+            return;
+        }
+
+        // set booleans
+        gripDown = myController.GetPressDown(gripBtn);
+        gripUp = myController.GetPressUp(gripBtn);
+        gripPressed = myController.GetPress(gripBtn);
+
+        triggerDown = myController.GetPressDown(triggerBtn);
+        triggerUp = myController.GetPressUp(triggerBtn);
+        triggerPressed = myController.GetPress(triggerBtn);
+
+        if (triggerDown)
+        {
+            // send ball out of room
+            enemyScript.Spawn();
+        }
+
+    }
+}
---
+++ PerceptionAlteration/Assets/_Scripts/Test/EnemyGenerator.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    // enemy classification
+    private enum EnemyType { smallest, small, large, upside };
+    private int currentType = 0; // default 0
+
+    public GameObject[] enemyPrefabs;
+
+    private List<GameObject> enemies;
+
+    // References to player/navmesh
+    private NavMeshAgent nav;         
+    
+    void Start ()
+    {
+        enemies = new List<GameObject>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (enemies != null)
+        {
+            foreach(GameObject g in enemies)
+            {
+                g.transform.localPosition += new Vector3(-0.1f, 0.0f, 0.0f);
+            }
+        }
+	
+	}
+    
+
+    // spawn random enemy
+    public void Spawn()
+    {
+        // rng god decides
+        currentType = Random.Range(0, 4);
+        GameObject newDog = Instantiate(enemyPrefabs[currentType], new Vector3(0f, 0f, 0f), Quaternion.identity) as GameObject;
+
+        // add parent
+        newDog.transform.parent = this.transform;
+
+        // add to list
+        enemies.Add(newDog);
+    }
+}
