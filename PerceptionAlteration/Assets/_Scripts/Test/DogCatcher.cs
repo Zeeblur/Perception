afdca27c2d0ef8c0c040e8df5bcb6e5249756775
Wall <40182161@live.napier.ac.uk>
2016-06-16 15:10:59 +0100

    Dog catcher catches dogs.

--- PerceptionAlteration/Assets/_Scripts/Test/DogCatcher.cs
+++ PerceptionAlteration/Assets/_Scripts/Test/DogCatcher.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class DogCatcher : MonoBehaviour
 {
+    public GameObject outerRing;
 
 	// Use this for initialization
 	void Start ()
     {
-	
 	}
 	
 	// Update is called once per frame
@@ -15,4 +15,11 @@ public class DogCatcher : MonoBehaviour
     {
 	
 	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        // when ball is caught rmeove from play + flash light
+        other.gameObject.SetActive(false);
+        outerRing.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+    }
 }
