facbd4388973b5db8ce4737f16f066f6bea71e0f
Wall <40182161@live.napier.ac.uk>
2016-06-29 19:28:40 +0100

    New scene with models and lighting. Rotating cube added and physics

---
+++ PerceptionAlteration/Assets/_Scripts/Plinths/RotatingCube.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingCube : MonoBehaviour
+{
+    public float speed;
+
+    void Update ()
+    {
+        transform.Rotate(new Vector3(0, 1, 0), speed);
+    }
+}
