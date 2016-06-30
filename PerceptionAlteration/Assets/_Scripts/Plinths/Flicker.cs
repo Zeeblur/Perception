33e678d45af1ae466182f9ae071ab8584e56b3d5
Wall <40182161@live.napier.ac.uk>
2016-06-30 17:37:05 +0100

    Light flicker and body started.

---
+++ PerceptionAlteration/Assets/_Scripts/Plinths/Flicker.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flicker : MonoBehaviour {
+
+    private Light spot;
+    public float speed;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        spot = GetComponent<Light>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Invoke("Toggle", speed*Time.deltaTime);
+	}
+
+    void Toggle()
+    {
+        spot.enabled = spot.enabled ? false : true;
+    }
+}
