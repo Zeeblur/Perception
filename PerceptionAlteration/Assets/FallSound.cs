91803cf37bb623badeedfa0eb5e57b831dce4845
Wall <40182161@live.napier.ac.uk>
2016-07-18 15:54:30 +0100

    Added woosh sound. Plinths collision sound and changed pitch/speed of cube rotation

---
+++ PerceptionAlteration/Assets/FallSound.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallSound : MonoBehaviour {
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        rb.Sleep();
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (!rb.IsSleeping())
+        {
+            AkSoundEngine.PostEvent("Play_Pillar", this.gameObject);
+        }
+    }
+}
