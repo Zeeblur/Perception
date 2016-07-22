75b4d35cea81bdc1858eba1a49bcd8e474eecd23
Wall <40182161@live.napier.ac.uk>
2016-07-22 15:29:40 +0100

    Added resources for UI. Need to add mechanics

--- PerceptionAlteration/Assets/SteamVR/Extras/SteamVR_GazeTracker.cs
+++ PerceptionAlteration/Assets/SteamVR/Extras/SteamVR_GazeTracker.cs
@@ -71,6 +71,7 @@ public class SteamVR_GazeTracker : MonoBehaviour
                     GazeEventArgs e;
                     e.distance = dist;
                     OnGazeOn(e);
+                    Debug.Log("Gazing");
                 }
                 else if (dist >= gazeOutCutoff && isInGaze)
                 {
